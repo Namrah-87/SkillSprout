fd551e587bf81b89664a80f5fc60165dd522092c
Mohid <namrahaijaz87@outlook.com>
2026-08-12 17:44:44 -0500

    stuff

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SkillSprout
+SkillSprout is a GT project by Mohid Atif for the 26-27 school year.
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
   console.log('SkillSprout loaded');
 });
+
